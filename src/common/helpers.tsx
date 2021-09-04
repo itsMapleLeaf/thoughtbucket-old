92e60822a84e039dc8eb405698408669e456187b
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-09-04 17:23:41 -0500

    add raise function

--- src/common/helpers.tsx
+++ src/common/helpers.tsx
@@ -1,3 +1,7 @@
 export function extractErrorMessage(value: unknown): string {
   return value instanceof Error ? value.message : String(value)
 }
+
+export function raise(message: string): never {
+  throw new Error(message)
+}
