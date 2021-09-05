b3b02e47f4e420dbfa0e3c30da5222f5b13e6b8b
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-09-04 19:08:15 -0500

    small fixes on clear button style

--- src/ui/button.ts
+++ src/ui/button.ts
@@ -8,6 +8,6 @@ export const solidButtonClass = clsx`
 `
 
 export const clearButtonClass = clsx`
-  p-3 -m-3 opacity-60 text-shadow
+  px-3 py-2 -mx-3 -my-2 opacity-60 text-shadow leading-none
   hover:opacity-100
 `
