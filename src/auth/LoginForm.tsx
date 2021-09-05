ef34d921d7f885b1eac48cfc88e48856046efae2
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-09-05 02:12:32 -0500

    pretty login

--- src/auth/LoginForm.tsx
+++ src/auth/LoginForm.tsx
@@ -1,7 +1,9 @@
 import React from "react"
 import { useMutation } from "react-query"
 import { extractErrorMessage } from "../common/helpers"
+import { Button } from "../dom/Button"
 import { supabase } from "../supabase/client"
+import { solidButtonClass } from "../ui/button"
 
 export function LoginForm() {
   const mutation = useMutation((variables: { email: string }) => {
@@ -17,15 +19,34 @@ export function LoginForm() {
   }
 
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault()
-        mutation.mutate({ email: event.currentTarget.email.value })
-      }}
-    >
-      <input name="email" type="email" />
-      <button type="submit">sign in with email</button>
-      {mutation.error && <p>{extractErrorMessage(mutation.error)}</p>}
-    </form>
+    <div>
+      <h1 className="text-3xl font-light">log in</h1>
+      <p className="mt-2">a magic link to log in will be sent to your email!</p>
+      <form
+        className="mt-4"
+        onSubmit={(event) => {
+          event.preventDefault()
+          mutation.mutate({ email: event.currentTarget.email.value })
+        }}
+      >
+        <label>
+          <div className="text-sm font-semibold">email</div>
+          <input
+            name="email"
+            type="email"
+            placeholder="your@email.com"
+            className="px-3 py-2 leading-none transition-colors bg-black/50 focus:outline-none focus:bg-black/75"
+          />{" "}
+        </label>
+        <Button
+          type="submit"
+          className={`${solidButtonClass} block mt-2`}
+          disabled={mutation.isLoading}
+        >
+          sign in with email
+        </Button>
+        {mutation.error && <p>{extractErrorMessage(mutation.error)}</p>}
+      </form>
+    </div>
   )
 }
